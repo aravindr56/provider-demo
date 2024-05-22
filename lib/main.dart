2cfed66f5bb918411ee92286b16c89591d5dadfd
aravindr56 <168095239+aravindr56@users.noreply.github.com>
2024-05-22 11:27:03 +0530

    initial

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provide_demo/provider/list_provider.dart';
+import 'package:provider/provider.dart';
+
+
+import 'first_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context)=>ListProvider()),
+      ],
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: FirstPage()
+      ),
+    );
+  }
+}
---
+++ lib/second_page.dart
@@ -0,0 +1,48 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provide_demo/provider/list_provider.dart';
+import 'package:provider/provider.dart';
+
+
+class SecondPage extends StatefulWidget {
+
+  const SecondPage({super.key});
+
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    ListProvider listProvider = Provider.of(context);
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: listProvider.list.length,
+                itemBuilder: (context, index) {
+                  return Text(listProvider.list[index].toString());
+                },
+              ),
+            ),
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context);
+            }, child: Text("First Page"))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          listProvider.addData();
+        },
+        child: Text("+"),
+      ),
+    );
+  }
+}
